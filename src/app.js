b9141fada7cc8cd3291de970690387100c0f335c
Gabrielviscio10 <gonzalo.viscio@icloud.com>
2024-07-24 18:38:30 +0000

    js terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateRandomCard() {
+    let suits = ["spade", "club", "heart", "diamond"];
+    let values = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    let randomValue = values[Math.floor(Math.random() * values.length)];
+
+    let card = document.getElementById("card");
+    card.className = "card " + randomSuit;
+    card.innerHTML = randomValue;
+
+    console.log("generateRandomCard");
+  }
+
+  generateRandomCard();
+
+  window.generateRandomCard = generateRandomCard;
 };
